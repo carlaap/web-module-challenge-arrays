6e58b02030b7ddd9e2d8a72eaa29e9d775620f9e
Byron Francisco Portillo <byronportillo@byrons-mbp.attlocal.net>
2022-03-09 21:16:01 -0800

    Completed task 5

--- index.js
+++ index.js
@@ -131,10 +131,10 @@ Use the getFlavorByIndex function below to do the following:
 */
 
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
-}
-
+function getFlavorByIndex(array, number){
+return array[number]
+};
+console.log(getFlavorByIndex(originalFlavors, 2));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
